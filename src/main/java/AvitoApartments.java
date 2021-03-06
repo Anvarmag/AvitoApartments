c34105507c427383587521bba3742c8507525e78
Anvarmag <Anvarfon@gmail.com>
2021-03-06 21:59:00 +0300

    Main.java added

--- src/main/java/AvitoApartments.java
+++ src/main/java/AvitoApartments.java
@@ -1,12 +1,18 @@
 public class AvitoApartments {
     long id;
-    String name;
+    String nameHC;
+    String nameMetro;
     String url;
     String image;
     String street;
+    String nameАgency;
+    boolean like;
     int room;
     double square;
     int floor;
+    int maxfloor;
     int price;
     double distance;
-    }
+    int publicationTime;
+}
+
